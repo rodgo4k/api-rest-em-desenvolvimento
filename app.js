10d05271a96425df104fbbba9f6ac7b812a15aa3
Rodrigo Prestes <rodrigoaprestes@gmail.com>
2020-07-30 15:19:09 -0300

    rest

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE< GET');
+        return res.status(200).send({});
+    }
+
+    next();
 });
 
 app.use('/produtos', rotaProdutos);
